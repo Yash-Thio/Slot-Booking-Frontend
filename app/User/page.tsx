869a49a3eabeff7e0a0735ab36275fd1d1682703
Yash Pandey <pandeyash06@gmail.com>
2024-10-06 17:16:37 +0530

    changed env

--- app/User/page.tsx
+++ app/User/page.tsx
@@ -2,22 +2,16 @@
 import React from "react";
 
 export default function Page() {
-  
-
-  return(
+  return (
     <div className="ml-12 h-full flex items-center justify-center">
       <div className="w-full">
-      <div className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-600 py-4">
-            <h1 className="text-7xl lg:text-7xl font-bold text-center text-opacity-80 text-black relative z-1">
-              Love Thy Self
-            </h1>
-            <h1 className="text-xl text-center text-opacity-80 font-bold">
-              
-            </h1>
-          </div>
+        <div className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-600 py-4">
+          <h1 className="text-7xl lg:text-7xl font-bold text-center text-opacity-80 text-black relative z-1">
+            Love Thy Self
+          </h1>
+          <h1 className="text-xl text-center text-opacity-80 font-bold"></h1>
+        </div>
       </div>
     </div>
-  
   );
 }
-
